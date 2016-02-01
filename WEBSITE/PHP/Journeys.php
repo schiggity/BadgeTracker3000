a857b152c9c89660b5c0ce139bdd6221599a33c3
ryschro <spug911@gmail.com>
2016-01-31 23:44:05 -0600

    started on journeys page

---
+++ WEBSITE/PHP/Journeys.php
@@ -0,0 +1,349 @@
+<?php include 'query.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Journeys</title> <!-- Change Page Tiltle Here -->
+  <!-- Stuff that is necessary for Bootstrap -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="TemplateCSS.css">
+</head>
+<body>
+
+<!---------------------------------------------------------------- NAV BAR STUFF -------------------------------------------------------->
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	  <img class="navbar-brand" src="girlscout2.png"></img>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Navigation <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#"></a></li>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="BadgeTracking.html">Badge Tracking</a></li>
+			<li><a href="BadgeRequirements.html">Badge Requirements</a></li>
+			<li><a href="BadgeOverview.html">Badge Overview</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Welcome User</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<!-------------------------------------------------------------- TABS STUFF ------------------------------------------------------------->
+<div class="container">
+        <div class="row-fluid">
+            <div class="col-md-12">
+                <h1>Journeys</h1>
+                <ul class="nav nav-tabs" id="myTab">
+                    <li class="active"><a href="#Tab1" data-toggle="tab">Daisies</a></li>
+                    <li><a href="#Tab2" data-toggle="tab">Brownies</a></li>
+                    <li><a href="#Tab3" data-toggle="tab">Juniors</a></li>
+                    <li><a href="#Tab4" data-toggle="tab">Cadettes</a></li>
+					<li><a href="#Tab5" data-toggle="tab">Seniors</a></li>
+					<li><a href="#Tab6" data-toggle="tab">Ambassadors</a></li>
+					
+                </ul>
+                <div class="tab-content my-tab">  <!-- Daisies tab and collapsing panel start -->
+                    <div class="tab-pane active" id="Tab1">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Daisies</h3>
+								<?php
+									$journeys = getJourneysByRank("daisy");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney($j["JID"]); ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="Tab2">  <!-- Brownies tab and collapsing panel start -->
+                        <div class="row">
+                            <div class="col-md-12">
+                               <h3>Brownies</h3>
+								<?php
+									$journeys = getJourneysByRank("brownie");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney($j["JID"]); ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="Tab3">  <!-- Juniors tab and collapsing panel start -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Juniors</h3>
+								<?php
+									$journeys = getJourneysByRank("junior");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney($j["JID"]); ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="Tab4">  <!-- Cadettes tab and collapsing panel start -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Cadettes</h3>
+								<?php
+									$journeys = getJourneysByRank("cadette");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney($j["JID"]); ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+					<div class="tab-pane" id="Tab5">  <!-- Seniors tab and collapsing panel start -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Seniors</h3>
+								<?php
+									$journeys = getJourneysByRank("senior");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney($j["JID"]); ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+					<div class="tab-pane" id="Tab6">  <!-- Ambassadors tab and collapsing panel start -->
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Ambassadors</h3>
+								<?php
+									$journeys = getJourneysByRank("ambassador");
+									foreach($journeys as $j){
+								?>
+								<div class="panel-group">
+									<div class="panel panel-default">
+										<div class="panel-heading">
+											<h4 class="panel-title">
+												<a data-toggle="collapse" href="#collapse<?php echo $j["JID"]; ?>"><?php echo $j["Name"]; ?></a>
+											</h4>
+										</div>
+										<div id="collapse<?php echo $j["JID"]; ?>" class="panel-collapse collapse">
+											<ul class="list-group">
+												<div class="container"> 
+													<p>***DESCRIPTION***</p>            
+														<table  style="width: 90%;">														
+															<tbody><?php $c = getScoutCountForJourney["JID"]; ?>
+																<tr>
+																<td>Number of Scouts Earned: <?php echo $c[1]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Awarded: <?php echo $c[2]; ?></td>																	
+																</tr>
+																<tr>
+																<td>Number of Scouts Started: <?php echo $c[0]; ?></td>																	
+																</tr>
+																<tr>														
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Specific Requirements</button> </td>
+																	<td align="right"> <button class="btn btn-secondary btn-lg">Update Records</button> </td>
+																</tr>
+															</tbody>
+														</table>
+												</div>
+											</ul>											
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- WEBSITE/PHP/query.php
+++ WEBSITE/PHP/query.php
@@ -8,7 +8,7 @@ include 'connect.php';
 function getAllBadges(){
 	global $conn;
 	$badgeArr = array();
-	$sql = "SELECT * FROM `badges`;";
+	$sql = "SELECT * FROM `badges` ORDER BY Name;";
 	
 	#fill array to be returned
 	$result = $conn->query($sql);
@@ -54,11 +54,10 @@ function getScoutCountForBadge($baid){
 	global $conn;
 	$NumComp = 0;
 	$QuestsCompleted = array();
-	$Startsql = "SELECT COUNT(DISTINCT sid) as started FROM scoutsdobadge WHERE BAID = " . $baid . ";";
+	$Startsql = "SELECT COUNT(DISTINCT sid) as started FROM scoutsdobadge WHERE baid = " . $baid . ";";
 	$Compsql  = "SELECT COUNT(DISTINCT baqid) as completed FROM scoutsdobadge JOIN badgequesthasrequirements ON scoutsdobadge.barid = badgequesthasrequirements.barid WHERE baid = " . $baid . " GROUP BY sid;";	
 	$QNsql	  = "SELECT COUNT(baqid) as questsNeeded FROM badgehasquest WHERE baid = " . $baid . ";";
 	$Awardsql = "SELECT COUNT(sid) as awarded FROM scoutsawardedbadge WHERE baid = " . $baid . ";";
-	
 	
 	#scouts who started/completed/awarded
 	$result = $conn->query($Startsql);
@@ -94,15 +93,15 @@ function getScoutCountForBadge($baid){
 	return array($started,$NumComp,$NumAward);	
 }
 
-#updates the scoutsDoBadge table when a requirement has been done \
+#updates the scoutsDoBadge table when a requirement has been done
 #--if no date is provided the function will default to the current date
-function completeReq($sid, $baid, $barid, $date){
+function completeBadgeReq($sid, $baid, $barid, $date){
 	global $conn;
 	if($date == 0){
 		$date = "NOW()";
 	}
 	$sql = "INSERT INTO scoutsdobadge VALUES(". $sid . "," . $baid . "," . $barid . "," . $date . ");";
-	echo $sql;
+	
 	if($result = $conn->query($sql)){
 		echo 'inserted';		
 	}
@@ -110,13 +109,203 @@ function completeReq($sid, $baid, $barid, $date){
 		echo $conn->error;
 	}
 }
+
+function getBadgesByRank($rank){
+	switch($rank){
+		case "Daisy":
+		case "daisy":
+		case "d":
+			$r = '10%';
+			break;
+		case "Brownie":
+		case "brownie":
+		case "b":
+			$r = '20%';
+			break;
+		case "Junior":
+		case "junior":
+		case "j":
+			$r = '30%';
+			break;
+		case "Cadette":
+		case "cadette":
+		case "c":
+			$r = '40%';
+			break;
+		case "Senior":
+		case "senior":
+		case "s":
+			$r = '50%';
+			break;
+		case "Ambassador":
+		case "ambassador":
+		case "a":
+			$r = '60%';
+			break;
+	}
+	
+	global $conn;
+	$badgeArr = array();
+	$sql = "select * from badges where BAID like '" . $r . "' ORDER BY Name;";
+	
+	#fill array to be returned
+	$result = $conn->query($sql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$badgeArr[$i] = $row;
+	}	
+	return $badgeArr;	
+}
 #endregion
 
 
 #region---------------------------JOURNEYS-----------------------------------
+function getAllJourneys(){
+	global $conn;
+	$jarr = array();
+	$sql = "SELECT * FROM journey ORDER BY Name;";
+	
+	#fill array to be returned
+	$result = $conn->query($sql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$jarr[$i] = $row;
+	}
+	
+	return $jarr;
+}
 
+function getRequirementsForJourney($jid){
+	global $conn;
+	$reqs = array();
+	$sql = "SELECT * from questrequirements where rid IN (select rid from questshasquestrequirements JOIN journeyhasquests ON questshasquestrequirements.qid = journeyhasquests.qid where jid = " . $jid . ");";
+	
+	#fill array to be returned
+	$result = $conn->query($sql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$reqs[$i] = $row;
+	}
+	return $reqs;
+}
 
+function getQuestsForJourney($jid){
+	global $conn;
+	$reqs = array();
+	$sql = "select * from quests where qid in (select qid from journeyhasquests where jid =" . $jid . ");";
+	
+	#fill array to be returned
+	$result = $conn->query($sql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$reqs[$i] = $row;
+	}	
+	return $reqs;	
+}
 
+function getScoutCountForJourney($jid){
+	global $conn;
+	$NumComp = 0;
+	$QuestsCompleted = array();
+	
+	$Startsql = "SELECT COUNT(DISTINCT sid) as started FROM scoutsdojourney WHERE jid = " . $jid . ";";
+	$Compsql  = "SELECT COUNT(DISTINCT qid) as completed FROM scoutsdojourney JOIN questshasquestrequirements ON scoutsdojourney.rid = questshasquestrequirements.rid WHERE jid = " . $jid . " GROUP BY sid;";	
+	$QNsql	  = "SELECT COUNT(qid) as questsNeeded FROM journeyhasquests WHERE jid = " . $jid . ";";
+	#SCOUTS ARE AWARDED QUESTS NOT JOURNEYS HELP!!!!!!!!!!!
+	$Awardsql = "SELECT COUNT(sid) as awarded FROM scoutsawardedquests WHERE qid = " . $jid . ";";
+	
+	#scouts who started/completed/awarded
+	$result = $conn->query($Startsql);
+	$started = $result->fetch_assoc()["started"];
+	
+	#number of quests completed by each scout for journey x
+	$result = $conn->query($Compsql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$QuestsCompleted[$i] = $row;
+	}
+	
+	#number of quests needed to complete journey x
+	$result = $conn->query($QNsql);
+	$QuestsNeeded = $result->fetch_assoc()["questsNeeded"];
+	
+	#number of scouts awarded journey x
+	$result = $conn->query($Awardsql);
+	$NumAward = $result->fetch_assoc()["awarded"];
+		
+	#comparing number of quests complete by each scout to quests needed for journey x
+	foreach ($QuestsCompleted as $quests){
+		if($quests == $QuestsNeeded){
+			$NumComp++;
+		}		
+	}
+	
+	#subtract scouts that have finished it
+	$started -= ($NumComp + $NumAward);
+	
+	#subtract scouts awarded from completed
+	$NumComp -= $NumAward;
+	
+	return array($started,$NumComp,$NumAward);	
+	
+}
+
+function completeJourneyReq($sid, $jid, $rid, $date){
+	global $conn;
+	if($date == 0){
+		$date = "NOW()";
+	}
+	$sql = "INSERT INTO scoutsdojourney VALUES(". $sid . "," . $baid . "," . $barid . "," . $date . ");";
+	
+	if($result = $conn->query($sql)){
+		echo 'inserted';		
+	}
+	else{
+		echo $conn->error;
+	}	
+}
+
+function getJourneysByRank($rank){
+	switch($rank){
+		case "Daisy":
+		case "daisy":
+		case "d":
+			$r = '10%';
+			break;
+		case "Brownie":
+		case "brownie":
+		case "b":
+			$r = '20%';
+			break;
+		case "Junior":
+		case "junior":
+		case "j":
+			$r = '30%';
+			break;
+		case "Cadette":
+		case "cadette":
+		case "c":
+			$r = '40%';
+			break;
+		case "Senior":
+		case "senior":
+		case "s":
+			$r = '50%';
+			break;
+		case "Ambassador":
+		case "ambassador":
+		case "a":
+			$r = '60%';
+			break;
+	}
+	
+	global $conn;
+	$jarr = array();
+	$sql = "select * from journey where jid like '" . $r . "' ORDER BY Name;";
+	
+	#fill array to be returned
+	$result = $conn->query($sql);
+	for($i = 0; $row = $result->fetch_assoc(); $i++){
+		$jarr[$i] = $row;
+	}	
+	return $jarr;	
+	
+}
 
 #endregion
 
